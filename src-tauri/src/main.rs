1a7fddfdc406f5e468ac760068d286881d77bdc6
5-23 <yhanbyeol6@gmail.com>
2024-05-09 22:52:54 +0900

    feat: config change

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -19,7 +19,7 @@ fn main() {
                 .expect("Unsupported platform! 'apply_vibrancy' is only supported on macOS");
 
             #[cfg(target_os = "windows")]
-            apply_blur(&window, Some((18, 18, 18, 125)))
+            apply_acrylic(&window, Some((18, 18, 18, 125)))
                 .expect("Unsupported platform! 'apply_blur' is only supported on Windows");
 
             Ok(())
