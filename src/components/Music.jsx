2929441d42bbcff7a18e77aaeef59ebf838447d3
5-23 <yhanbyeol6@gmail.com>
2024-08-12 22:29:52 +0900

    fix: ios bug

--- src/components/Music.jsx
+++ src/components/Music.jsx
@@ -1,90 +1,90 @@
-export default function Music({musicEntry, onClick, q, playing, savedMusic}) {
-	let [name, music] = musicEntry;
-	var [savedMusic, setSavedMusic] = savedMusic
-	
-	let pattern = q.split('').join('\\s*');
-	let regex = new RegExp(pattern, 'gi');
-	let wrappedName = name.replace(regex, match => `<b>${match}</b>`);
-	
-	if (q == "") {
-		wrappedName = name;
-	}
-	if (name == playing)
-		wrappedName = `<p>${wrappedName}</p>`;
+export default function Music({ musicEntry, onClick, q, playing, savedMusic }) {
+    let [name, music] = musicEntry;
+    var [savedMusic, setSavedMusic] = savedMusic
 
-	return <div className="music" q={q}>
-		<div onClick={() => {onClick(name)}}>
-			<img src={music.image} />
-			<span dangerouslySetInnerHTML={{ __html: wrappedName }} />
-		</div>
+    let pattern = q.split('').join('\\s*');
+    let regex = new RegExp(pattern, 'gi');
+    let wrappedName = name.replace(regex, match => `<b>${match}</b>`);
 
-		{
-				Object.entries(savedMusic).map(e => e[0]).indexOf(name) != -1? <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" onClick={() => {
-					const dbOpen = indexedDB.open("ny-music");
-					dbOpen.onsuccess = () => {
-						const db = dbOpen.result;
-						/** @type {IDBTransaction} */
-						
-						const transaction = db.transaction(["musics"], "readwrite");
-						const musics = transaction.objectStore("musics");
-						const request = musics.delete(name);
+    if (q == "") {
+        wrappedName = name;
+    }
+    if (name == playing)
+        wrappedName = `<p>${wrappedName}</p>`;
 
-						let pAsta = {...savedMusic}
-						delete pAsta[name]
-						setSavedMusic(pAsta);
-				
-						request.onsuccess = e => {
-							console.log("success");
-						};
+    return <div className="music" q={q}>
+        <div onClick={() => { onClick(name) }}>
+            <img src={music.image} />
+            <span dangerouslySetInnerHTML={{ __html: wrappedName }} />
+        </div>
 
-					};
-				}}>
-					<path fill="var(--primary)" d="M256 512a256 256 0 1 0 0-512 256 256 0 1 0 0 512zm113-303L241 337c-9.4 9.4-24.6 9.4-33.9 0l-64-64c-9.4-9.4-9.4-24.6 0-33.9s24.6-9.4 33.9 0l47 47L335 175c9.4-9.4 24.6-9.4 33.9 0s9.4 24.6 0 33.9z" />
-				</svg>:
-				<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" onClick={() => {
-					const dbOpen = indexedDB.open("ny-music");
+        {
+            Object.entries(savedMusic).map(e => e[0]).indexOf(name) != -1 ? <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" onClick={() => {
+                const dbOpen = indexedDB.open("ny-music");
+                dbOpen.onsuccess = () => {
+                    const db = dbOpen.result;
+                    /** @type {IDBTransaction} */
 
-					dbOpen.onsuccess = () => {
-						const db = dbOpen.result;
-						/** @type {IDBTransaction} */
-						// const transaction = db.transaction("musics", "readwrite");
-						// const musics = transaction.objectStore("musics");
-						fetch(`https://f.imnyang.xyz/NY64_Cover/Cover/${name}.mp3`)
-							.then(res => res.blob())
-							.then(cover => {
-								fetch(`https://f.imnyang.xyz/NY64_Cover/Image/${name}.webp`)
-									.then(res => res.blob())
-									.then(image => {
-										const transaction = db.transaction(["musics"], "readwrite");
-										const musics = transaction.objectStore("musics");
-										const request = musics.put({
-											name: name,
-											cover: cover,
-											image: image,
-										});
+                    const transaction = db.transaction(["musics"], "readwrite");
+                    const musics = transaction.objectStore("musics");
+                    const request = musics.delete(name);
 
-										let pAsta = {...savedMusic}
-										pAsta[name] = {
-											image: URL.createObjectURL(image),
-											cover: URL.createObjectURL(cover)
-										}
-										setSavedMusic(pAsta);
-								
-										request.onsuccess = e => {
-											console.log("success");
-										};
-									})
-							}).catch(error => {
-							console.error('Error:', error);
-						});
+                    let pAsta = { ...savedMusic }
+                    delete pAsta[name]
+                    setSavedMusic(pAsta);
 
-					};
-				}}>
-					<path fill="var(--gray)" d="M256 0a256 256 0 1 0 0 512 256 256 0 1 0 0-512zM127 281c-9.4-9.4-9.4-24.6 0-33.9s24.6-9.4 33.9 0l71 71L232 136c0-13.3 10.7-24 24-24s24 10.7 24 24v182.1l71-71c9.4-9.4 24.6-9.4 33.9 0s9.4 24.6 0 33.9L273 393c-9.4 9.4-24.6 9.4-33.9 0L127 281z" />
-				</svg>
-		}
+                    request.onsuccess = e => {
+                        console.log("success");
+                    };
 
-			
+                };
+            }}>
+                <path fill="var(--primary)" d="M256 512a256 256 0 1 0 0-512 256 256 0 1 0 0 512zm113-303L241 337c-9.4 9.4-24.6 9.4-33.9 0l-64-64c-9.4-9.4-9.4-24.6 0-33.9s24.6-9.4 33.9 0l47 47L335 175c9.4-9.4 24.6-9.4 33.9 0s9.4 24.6 0 33.9z" />
+            </svg> :
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" onClick={() => {
+                    const dbOpen = indexedDB.open("ny-music");
 
-	</div>
+                    dbOpen.onsuccess = () => {
+                        const db = dbOpen.result;
+                        /** @type {IDBTransaction} */
+                        // const transaction = db.transaction("musics", "readwrite");
+                        // const musics = transaction.objectStore("musics");
+                        fetch(`https://f.imnyang.xyz/NY64_Cover/Cover/${name}.mp3`)
+                            .then(res => res.blob())
+                            .then(cover => {
+                                fetch(`https://f.imnyang.xyz/NY64_Cover/Image/${name}.webp`)
+                                    .then(res => res.blob())
+                                    .then(image => {
+                                        const transaction = db.transaction(["musics"], "readwrite");
+                                        const musics = transaction.objectStore("musics");
+                                        const request = musics.put({
+                                            name: name,
+                                            cover: cover,
+                                            image: image,
+                                        });
+
+                                        let pAsta = { ...savedMusic }
+                                        pAsta[name] = {
+                                            image: URL.createObjectURL(image),
+                                            cover: URL.createObjectURL(cover)
+                                        }
+                                        setSavedMusic(pAsta);
+
+                                        request.onsuccess = e => {
+                                            console.log("success");
+                                        };
+                                    })
+                            }).catch(error => {
+                                console.error('Error:', error);
+                            });
+
+                    };
+                }}>
+                    <path fill="var(--gray)" d="M256 0a256 256 0 1 0 0 512 256 256 0 1 0 0-512zM127 281c-9.4-9.4-9.4-24.6 0-33.9s24.6-9.4 33.9 0l71 71L232 136c0-13.3 10.7-24 24-24s24 10.7 24 24v182.1l71-71c9.4-9.4 24.6-9.4 33.9 0s9.4 24.6 0 33.9L273 393c-9.4 9.4-24.6 9.4-33.9 0L127 281z" />
+                </svg>
+        }
+
+
+
+    </div>
 }
--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,8 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
+const host = process.env.TAURI_DEV_HOST;
+
 // https://vitejs.dev/config/
 export default defineConfig(async () => ({
   plugins: [react()],
@@ -13,6 +15,14 @@ export default defineConfig(async () => ({
   server: {
     port: 1420,
     strictPort: true,
+    host: host || false,
+    hmr: host
+      ? {
+          protocol: "ws",
+          host,
+          port: 1421,
+        }
+      : undefined,
     watch: {
       // 3. tell vite to ignore watching `src-tauri`
       ignored: ["**/src-tauri/**"],
